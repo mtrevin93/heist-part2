1dc3fa4044ec00fa0c3251ab56e11249954bd482
Michael Trevino <mtrevin93@gmail.com>
2021-08-30 15:39:45 -0400

    phase5 complete

--- princess-brides/Program.cs
+++ princess-brides/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace princess_brides
 {
@@ -7,10 +6,13 @@ namespace princess_brides
     {
         static void Main(string[] args)
         {
+            Bank bank = new Bank();
+            Console.WriteLine("Bank Difficulty Level: ");
+            bank.difficultyLevel = int.Parse(Console.ReadLine());
             Console.WriteLine("Plan your Heist!");
 
             Team HeistTeam = new Team();
-
+            //Run until user does not enter name
             while (true)
             {
                 {
@@ -41,31 +43,30 @@ namespace princess_brides
             Console.WriteLine("Number of trial runs?");
             int numRuns = int.Parse(Console.ReadLine());
             int Total = 0;
+            int successes = 0;
+            int failures = 0;
             foreach (Member member in HeistTeam.TeamMembers)
             {
                 Total += member.SkillLevel;
             }
-            Bank bank = new Bank();
+            //Runs randomized trial runs
             for (int i = 0; i < numRuns; i++)
-            {
-            int thisRunsDifficulty = bank.newLuckValue();
-            if (Total >= thisRunsDifficulty)
-            {
-                Console.WriteLine($"The team's combined skill level: {Total}. The bank's difficulty level: {thisRunsDifficulty}.");
-                Console.WriteLine("Success!");
+                {
+                int thisRunsDifficulty = bank.newLuckValue();
+                if (Total >= thisRunsDifficulty)
+                {
+                    Console.WriteLine($"The team's combined skill level: {Total}. The bank's difficulty level: {thisRunsDifficulty}.");
+                    Console.WriteLine("Success!");
+                    successes++;
+                }
+                else
+                {
+                    Console.WriteLine($"The team's combined skill level: {Total}. The bank's difficulty level: {thisRunsDifficulty}.");
+                    Console.WriteLine("Failure!");
+                    failures++;
+                }
             }
-            else
-            {
-                Console.WriteLine($"The team's combined skill level: {Total}. The bank's difficulty level: {thisRunsDifficulty}.");
-                Console.WriteLine("Failure!");
-            }
-            }
+             Console.WriteLine($"Successes: {successes} Failures: {failures}");
         }
     }
 }
-
-// Phase Six
-// The program should be updated to...
-
-// At the beginning of the program, prompt the user to enter the difficulty level of the bank.
-// At the end of the program, display a report showing the number of successful runs and the number of failed runs.
