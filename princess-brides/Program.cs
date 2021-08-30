fd6323508775d887c17427146d652c14e06ddf95
averynoble <averynoble.an@gmail.com>
2021-08-30 10:15:51 -0500

    Finished phase 1 verifiying that member displayed

---
+++ princess-brides/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace princess_brides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your Heist!");
+
+            Console.WriteLine("Please enter your team members name!");
+            Member member = new Member();
+
+            member.Name = Console.ReadLine();
+
+            Console.WriteLine("Please enter their skill level!");
+            member.SkillLevel = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter their courage factor!");
+            member.CourageFactor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(member.DisplayMember(member));
+
+            
+        }
+    }
+}
---
+++ princess-brides/member.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace princess_brides
+{
+    public class Member
+    {
+        public string Name { get; set;}
+
+        public int SkillLevel { get; set; }
+
+        public float CourageFactor { get; set; }
+
+        public Member()
+        {
+            
+        }
+
+        public string DisplayMember(Member member)
+        {
+           return (
+            $"Name - {member.Name}, Skill Level - {member.SkillLevel},  Courage Factor - {member.CourageFactor}"
+               );
+        }
+    }
+}
